985a69abc8a2c680f358d0b376c95007c106ce87
Langen-Tardigrada <surting.st@gmail.com>
2021-01-29 10:44:38 +0700

    run graph

---
+++ graph_turnAR.py
@@ -0,0 +1,37 @@
+#%%
+import matplotlib.pyplot as plotG
+from pandas import DataFrame
+
+turn_gp_FCFS= []
+
+turn_gp_SJF = []
+
+turn_gp_RR = []
+
+# import file "./" run with python
+# import filr "../" run with jupyter
+f = open("../data_of_time/FCFS/sixtyTurnARTime.txt","r")
+for i in f:
+    turn_gp_FCFS.append(int(i.strip("\n")))
+f.close()
+
+f = open("../data_of_time/SJF/sixtyTurnARTime.txt","r")
+for i in f:
+    turn_gp_SJF.append(int(i.strip("\n")))
+f.close()
+
+f = open("../data_of_time/RR/sixtyTurnARTime.txt","r")
+for i in f:
+    turn_gp_RR.append(int(i.strip("\n")))
+f.close()
+
+Data= {'turn around time(FCFS)': turn_gp_FCFS, 'turn around time(SJF)': turn_gp_SJF,'turn around time(RR)': turn_gp_RR}
+dataF = DataFrame(Data,columns=['turn around time(FCFS)','turn around time(SJF)','turn around time(RR)'])
+dataF.plot(kind="line")
+plotG.xlabel('processes')
+plotG.ylabel('waiting time')
+plotG.title('60 processes')
+plotG.grid(True)
+
+
+# %%
---
+++ graph_waiting.py
@@ -0,0 +1,35 @@
+#%%
+import matplotlib.pyplot as plotG
+from pandas import DataFrame
+
+wait_gp_FCFS = []
+
+wait_gp_SJF = []
+
+wait_gp_RR = []
+
+# import file "./" run with python
+# import filr "../" run with jupyter
+f = open("../data_of_time/FCFS/sixtyWaitingTime.txt","r")
+for i in f:
+    wait_gp_FCFS.append(int(i.strip("\n")))
+f.close()
+
+f = open("../data_of_time/SJF/sixtyWaitingTime.txt","r")
+for i in f:
+    wait_gp_SJF.append(int(i.strip("\n")))
+f.close()
+
+f = open("../data_of_time/RR/sixtyWaitingTime.txt","r")
+for i in f:
+    wait_gp_RR.append(int(i.strip("\n")))
+f.close()
+
+Data= {'waiting time(FCFS)': wait_gp_FCFS, 'waiting time(SJF)': wait_gp_SJF,'waiting time(RR)': wait_gp_RR}
+dataF = DataFrame(Data,columns=['waiting time(FCFS)','waiting time(SJF)','waiting time(RR)'])
+dataF.plot(kind="line")
+plotG.xlabel('processes')
+plotG.ylabel('waiting time')
+plotG.title('60 processes')
+plotG.grid(True)
+
